8851c4e894b72a76744eedbe701a65e2d0683e1c
mjcumming <mike@4831.com>
2019-05-31 14:38:35 -0500

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(name='ISY994v5',
-      version='0.0.5',
+      version='0.0.6',
       description='ISY99 Controller Rest and Websocket client v5 firmware',
       author='Michael Cumming',
       author_email='mike@4831.com',
